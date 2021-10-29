71db2994ad4d66ed097744b26e69ebb344163a33
Mohamed-Hammoud Ahmed Chokor <mohameddd701@gmail.com>
2021-10-29 20:56:24 +0300

    Added Unknown Face Labling

--- FaceAttendance.py
+++ FaceAttendance.py
@@ -58,12 +58,13 @@ while True:
         if matches[matchIndex]:
             name = classNames[matchIndex].upper()
             # print(name)
-            y1,x2,y2,x1 = faceLoc
-            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
-            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
-            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
-            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,0.6,(255,255,255),2)
             markAttendance(name)
+        else: name = 'Unknown'
+        y1,x2,y2,x1 = faceLoc
+        y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+        cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+        cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+        cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,0.6,(255,255,255),2)
     
     cv2.imshow('CMPS327 - Attendance Cam',img)
     cv2.waitKey(1)
