0e86d271943f23d6c557964fb8a7372a35e70e5c
Mohamed-Hammoud Ahmed Chokor <mohameddd701@gmail.com>
2021-10-29 12:19:37 +0300

    Personalized Code

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ import numpy as np
 import face_recognition
 import os
 from datetime import datetime
-# from PIL import ImageGrab
  
 path = 'ImagesAttendance'
 images = []
@@ -36,11 +35,6 @@ def markAttendance(name):
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
  
-#### FOR CAPTURING SCREEN RATHER THAN WEBCAM
-# def captureScreen(bbox=(300,300,690+300,530+300)):
-#     capScr = np.array(ImageGrab.grab(bbox))
-#     capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
-#     return capScr
  
 encodeListKnown = findEncodings(images)
 print('Encoding Complete')
@@ -49,7 +43,6 @@ cap = cv2.VideoCapture(0)
  
 while True:
     success, img = cap.read()
-    #img = captureScreen()
     imgS = cv2.resize(img,(0,0),None,0.25,0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
     
@@ -72,5 +65,5 @@ while True:
             cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,0.6,(255,255,255),2)
             markAttendance(name)
     
-    cv2.imshow('Webcam',img)
+    cv2.imshow('CMPS327 - Attendance Cam',img)
     cv2.waitKey(1)
